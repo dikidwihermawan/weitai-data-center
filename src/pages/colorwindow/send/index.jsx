72c0a3fc3974a2b4d046bac91a8836a742fa404e
Diki Dwi Hermawan <dikidwihermawan@gmail.com>
2024-08-15 16:43:14 +0700

    update send color window

--- src/pages/colorwindow/send/index.jsx
+++ src/pages/colorwindow/send/index.jsx
@@ -5,42 +5,21 @@ import { IconSquareRoundedCheck, IconTrash } from "@tabler/icons-react";
 import { useNavigate } from "react-router-dom";
 import swal from "sweetalert";
 import ColorWindow from "..";
+import Modal from "react-modal";
 
 function SendColorWindow() {
   const redirect = useNavigate();
   const [colorWindows, setColorWindows] = useState([]);
   const [searchInput, setSearchInput] = useState("");
   const [filteredResults, setFilteredResults] = useState([]);
-  const [showModal, setShowModal] = useState(true);
+  const [modalIsOpen, setIsOpen] = useState(false);
+  const [dataUpdate, setDataUpdate] = useState("");
+  const [returned, setReturned] = useState("");
+  let subtitle;
 
   const getData = async () => {
     const response = await axios.get("colorwindow/send");
     setColorWindows(response.data.data);
-  };
-
-  const confirmData = (id) => {
-    swal({
-      title: "Are you sure?",
-      text: "Do you want to confirm data?",
-      icon: "info",
-      buttons: true,
-      dangerMode: false,
-    }).then(async (willDelete) => {
-      if (willDelete) {
-        try {
-          const response = await axios.delete(`colorwindow/send/delete/${id}`);
-          swal(response.data.success, {
-            icon: "success",
-          });
-          getData();
-        } catch (e) {
-          swal("Data can't deleted!", {
-            icon: "error",
-          });
-          console.log(e);
-        }
-      }
-    });
   };
   const deleteData = (id) => {
     swal({
@@ -69,7 +48,8 @@ function SendColorWindow() {
 
   const handleClick = (id, action) => {
     if (action == "confirmed") {
-      confirmData(id);
+      openModal();
+      setDataUpdate(id);
     } else if (action == "delete") {
       deleteData(id);
     }
@@ -154,9 +134,7 @@ function SendColorWindow() {
     return (
       <div className="flex items-center space-x-4">
         <button
-          onClick={() => {
-            setShowModal(false);
-          }}
+          onClick={() => handleClick(id, "confirmed")}
           className="px-2 py-2 text-xs rounded"
           title="Selesai"
         >
@@ -185,138 +163,108 @@ function SendColorWindow() {
     }
   };
 
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    subtitle.style.color = "#f00";
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+  const customStyles = {
+    content: {
+      width: "30%",
+      top: "50%",
+      left: "50%",
+      right: "auto",
+      bottom: "auto",
+      marginRight: "-50%",
+      borderRadius: "20px",
+      transform: "translate(-50%, -50%)",
+    },
+  };
+
+  const confirmData = () => {
+    swal({
+      title: "Are you sure?",
+      text: "Do you want to confirm data?",
+      icon: "info",
+      buttons: true,
+      dangerMode: false,
+    }).then(async (willDelete) => {
+      if (willDelete) {
+        try {
+          const response = await axios.post(
+            `colorwindow/send/confirm/${dataUpdate}`,
+            { returned: returned }
+          );
+          swal(response.data.success, {
+            icon: "success",
+          });
+          closeModal();
+          getData();
+        } catch (e) {
+          swal("Data can't confirmed!", {
+            icon: "error",
+          });
+          console.log(e);
+        }
+      }
+    });
+  };
+
   useEffect(() => {
     getData();
     handleInputChange();
-    console.log(showModal);
-  }, [searchInput, showModal]);
+  }, [searchInput]);
 
   return (
     <>
       <ColorWindow tabActive="send">
-        <>
-          {/* Main modal */}
-          <div
-            id="authentication-modal"
-            tabIndex={-1}
-            aria-hidden="true"
-            className={`${
-              showModal ? "hidden" : null
-            } overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full"`}
-          >
-            <div className="relative p-4 w-full max-w-md max-h-full">
-              {/* Modal content */}
-              <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
-                {/* Modal header */}
-                <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
-                  <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
-                    Sign in to our platform
-                  </h3>
-                  <button
-                    type="button"
-                    className="end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
-                    data-modal-hide="authentication-modal"
-                  >
-                    <svg
-                      className="w-3 h-3"
-                      aria-hidden="true"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 14 14"
-                    >
-                      <path
-                        stroke="currentColor"
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth={2}
-                        d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
-                      />
-                    </svg>
-                    <span className="sr-only">Close modal</span>
-                  </button>
-                </div>
-                {/* Modal body */}
-                <div className="p-4 md:p-5">
-                  <form className="space-y-4" action="#">
-                    <div>
-                      <label
-                        htmlFor="email"
-                        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                      >
-                        Your email
-                      </label>
-                      <input
-                        type="email"
-                        name="email"
-                        id="email"
-                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-                        placeholder="name@company.com"
-                        required=""
-                      />
-                    </div>
-                    <div>
-                      <label
-                        htmlFor="password"
-                        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                      >
-                        Your password
-                      </label>
-                      <input
-                        type="password"
-                        name="password"
-                        id="password"
-                        placeholder="••••••••"
-                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-                        required=""
-                      />
-                    </div>
-                    <div className="flex justify-between">
-                      <div className="flex items-start">
-                        <div className="flex items-center h-5">
-                          <input
-                            id="remember"
-                            type="checkbox"
-                            defaultValue=""
-                            className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-600 dark:border-gray-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
-                            required=""
-                          />
-                        </div>
-                        <label
-                          htmlFor="remember"
-                          className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-                        >
-                          Remember me
-                        </label>
-                      </div>
-                      <a
-                        href="#"
-                        className="text-sm text-blue-700 hover:underline dark:text-blue-500"
-                      >
-                        Lost Password?
-                      </a>
-                    </div>
-                    <button
-                      type="submit"
-                      className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                    >
-                      Login to your account
-                    </button>
-                    <div className="text-sm font-medium text-gray-500 dark:text-gray-300">
-                      Not registered?{" "}
-                      <a
-                        href="#"
-                        className="text-blue-700 hover:underline dark:text-blue-500"
-                      >
-                        Create account
-                      </a>
-                    </div>
-                  </form>
-                </div>
-              </div>
-            </div>
+        <Modal
+          isOpen={modalIsOpen}
+          onAfterOpen={afterOpenModal}
+          onRequestClose={closeModal}
+          style={customStyles}
+          contentLabel="confirmModal"
+        >
+          <div className="flex items-center justify-between p-4 md:px-5 border-b rounded-t dark:border-gray-600">
+            <h3 className="text-xs font-semibold text-gray-900 dark:text-white">
+              Kembalikan Color Window
+            </h3>
           </div>
-        </>
-
+          {/* Modal body */}
+          <div className="p-4 md:p-5">
+            <div className="mb-4">
+              <label
+                htmlFor="returned"
+                className="block mb-2 text-xs font-medium text-gray-900 dark:text-white"
+              >
+                Tanggal Kembali
+              </label>
+              <input
+                type="date"
+                name="returned"
+                id="returned"
+                onChange={(e) => setReturned(e.target.value)}
+                value={returned}
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-xs rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+              />
+            </div>
+            <button
+              type="button"
+              onClick={confirmData}
+              className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-xs px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              Confirm
+            </button>
+          </div>
+        </Modal>
         <DataTable
           columns={columns}
           data={searchInput != "" ? filteredResults : colorWindows}
